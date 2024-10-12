43b312bc4c84c9205eaffe3bd9766885c3c23c78
Matthew Sharp <matthew.sharp1@ontariotechu.net>
2024-10-12 13:33:15 -0400

    Modified File

--- utils/browserSetup.py
+++ utils/browserSetup.py
@@ -3,10 +3,11 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+# Function sets up Chrome WebDriver using ChromeDriverManager
+# Return: Driver - Chrome WebDriver configured with provided options
 def get_browser():
-    # Setup Chrome WebDriver using ChromeDriverManager
     options = Options()
-    options.add_experimental_option("detach", True)
-    options.add_argument("--start-maximized")  # Start browser maximized
+    options.add_experimental_option("detach", True)     # Browser will stay open after completion
+    options.add_argument("--start-maximized")           # Start browser maximized
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     return driver
