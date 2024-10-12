6a36a4da5bee6a0b74169a23da36818d2095d797
Matthew Sharp <matthew.sharp1@ontariotechu.net>
2024-10-12 13:40:41 -0400

    Added Comments

--- tests/testAirCanada.py
+++ tests/testAirCanada.py
@@ -1,24 +1,23 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 import time
-
 import pytest
 from utils.browserSetup import get_browser
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+
+# This file will run the test to see if the website has loaded correctly
 
 # Base URL for Air Canada website
 BASE_URL = "https://www.aircanada.com/"
 
+# This function initializes test function to be called during runtime
+# Return: Driver - Returns driver setup from browserSetup.py and gets website URL
 @pytest.fixture
 def setup():
     driver = get_browser()
     driver.get(BASE_URL)
     yield driver
 
+# This function will test to see if the page title matches the desired string
+# to ensure the correct page has loaded
 def test_home_page_title(setup):
-    driver = setup
-    time.sleep(5)
-    assert 'Book Flights Online | Air Canada' in driver.title
+    driver = setup                                              # Call the setup fixture
+    time.sleep(5)                                               # Wait 5 seconds for website to load
+    assert 'Book Flights Online | Air Canada' in driver.title   # Assert Test Case
