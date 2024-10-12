1d67acd12b3da281b17278ac0f1baa67cb3a25b8
Matthew Sharp <matthew.sharp1@ontariotechu.net>
2024-10-12 14:12:06 -0400

    Added Comments

--- tests/testAirCanadaHelpCarryOnLiquids.py
+++ tests/testAirCanadaHelpCarryOnLiquids.py
@@ -1,66 +1,70 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 import time
-
 import pytest
 from utils.browserSetup import get_browser
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+
+# This file will run the test to see the answer in the Help page regarding bringing liquids through security
+# in your carry-on
 
 # Base URL for Air Canada website
 BASE_URL = "https://www.aircanada.com/"
 
+# This function initializes test function to be called during runtime
+# Return: Driver - Returns driver setup from browserSetup.py and gets website URL
 @pytest.fixture
 def setup():
     driver = get_browser()
     driver.get(BASE_URL)
     yield driver
 
+# This function will traverse the website and help formums, switching tabs in the process
 def test_contact_us_page_access(setup):
+    # Setup Driver
     driver = setup
-    
+    # Wait 5 Seconds for Website to Load
     time.sleep(5)
 
-    # Scroll to the bottom of the page to find "Contact Us"
+    # Scroll to the bottom of the page to find "Help and Contact"
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     
-    time.sleep(3)
+    # Wait for Window to Scroll
+    time.sleep(1)
 
+    # Locate and Click the "Help and Contact" link
     contact_us_link = driver.find_element(By.LINK_TEXT, "Help And Contact")
     contact_us_link.click()
 
+    # Wait 5 Seconds for Window to Load
     time.sleep(5)
-
+    # Switch to new Window
     driver.switch_to.window(driver.window_handles[1])
 
-    time.sleep(3)
-
+    # Locate and Click the element the first element with "View all"
     page_traversal = driver.find_element(By.XPATH, "//a[text()[contains(., 'View all')]]")
     page_traversal.click()
 
-    time.sleep(2)
-
+    # Wait 1 second for element to appear
+    time.sleep(1)
+    # Scroll down the page
     driver.execute_script("window.scrollTo(0, 250);")
 
-    time.sleep(2)
+    # Wait for window to scroll
+    time.sleep(1)
 
     page_traversal = driver.find_element(By.XPATH, "//a[text()[contains(., 'Carry-on baggage')]]")
     page_traversal.click()
 
+    # Wait for New Window To Load
     time.sleep(5)
-
+    # Switch to New Window
     driver.switch_to.window(driver.window_handles[2])
 
-    time.sleep(3)
-
+    # Locate and Click on the "Liquids and Gels Tab"
     liquids_tab = driver.find_element(By.XPATH, "//ul[contains(@id, 'tabs_header_6511')][.//li[contains(@id, 'tab_6511_title_1')]][.//span]")
     liquids_tab.click()
 
-    time.sleep(2)
-
+    # Retrieve the Desired Element
     answer = driver.find_element(By.XPATH, "//div[.//p][.//strong]")
     
-    # Verify if "Contact Us" page loaded
+    # Verify the answer to the customers query using the innerHTML
     assert "Always purchase liquids after you’ve passed the security checkpoint." in answer.get_attribute('innerHTML')
